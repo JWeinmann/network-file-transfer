8dcef9d35a181f3bd786846bb0de241d0b72414c
jweinmann <weinmann@uleth.ca>
2019-11-20 11:43:07 -0700

    value and segment constraints for Header.setSegment

--- Header.py
+++ Header.py
@@ -1,52 +1,56 @@
 # kill -9 "will be your friend"
 
 '''
-going to need to setup some strict limitations on the values set
+going to need to setup some strict limitations on the values for setSegment
 '''
 class Header:
-    ''' raw bytes for header'''
-    __header = bytearray(47)
 
-    ''' offsets for the various sections of the header '''
-    __offsets = {
-        "SEQ": 0,
-        "ACK": 4,
-        "LENGTH": 8,
-        "WINDOW": 10,
-        "FLAGS": 14,
-        "SHA": 15,
-        "DATA": 47
-    }
-
-    ''' size of each segment of the header '''
-    __sizes = {
-        "SEQ": 4,
-        "ACK": 4,
-        "LENGTH": 2,
-        "WINDOW": 3, ''' *********double check this *********** '''
-        "FLAGS": 1,
-        "SHA": 32,
-    }
-
-    #def __init__(self):
-        #self.__header =
-        #self.__FLAGS = bytes.fromhex('00')
+    def __init__(self) -> None:
+        self.__header = __header = bytearray(47)
+        ''' offsets for the various sections of the header '''
+        self.__offsets = {
+            "SEQ": 0,
+            "ACK": 4,
+            "LENGTH": 8,
+            "WINDOW": 10,
+            "FLAGS": 12,
+            "SHA": 13,
+            "DATA": 45
+        }
+        ''' size of each segment of the header '''
+        self.__sizes = {
+            "SEQ": 4,
+            "ACK": 4,
+            "LENGTH": 2,
+            "WINDOW": 2,
+            "FLAGS": 1,
+            "SHA": 32,
+            "DATA": 0 # default packet has no data
+        }
 
     def header(self) -> bytearray:
         return self.__header
 
     ''' set a segment '''
-    ''' used for all segments EXCEPT flags and data '''
+    ''' used for all segments EXCEPT flags, data, and sha256'''
     ''' segment ex: "ACK", or "LENGTH", or "SHA" '''
-    ''' value can be an int or hex, ex: 2914, or 0x9a'''
-    def setSegment(self, segment: str, value) -> None:
-        for b, i in zip(range( self.__offsets[segment], self.__offsets[segment] + self.__sizes[segment]    ), range(0, 9**99)    ):
-            ''' need to finish this constraint on lengths '''
-            if value > 2**(8*self.__sizes[segment]):
-                #raise: Exception(f"The provided value is {len(bstr)} bytes. The value for {segment} cannot be larger than {self._sizes[segment]} bytes.")
-                raise Exception(f'value {self.__sizes[segment]} too large')
+    ''' value ex: 2914, or 0x9a'''
+    def setSegment(self, segment: str, value: int) -> None:
+        segment = segment.upper()
+        ''' segment must be valid '''
+        if segment not in ["SEQ","ACK","LENGTH","WINDOW","FLAGS","SHA","DATA"]:
+            raise Exception(f'\"{segment}\" is not a valid segment')
+        elif segment in ["FLAGS","SHA","DATA"]:
+            raise Exception(f'\"{segment}\" cannot be set using Header.setSegment().')
 
-            '''convert num to bytes() object'''
+        ''' constrain value to what's required for segment '''
+        if value > 2**(8*self.__sizes[segment]) | value < 0:
+            #raise: Exception(f"The provided value is {len(bstr)} bytes. The value for {segment} cannot be larger than {self._sizes[segment]} bytes.")
+            raise Exception(f'Invalid value for {segment}. Must be  between 0 and {self.__sizes[segment]}.')
+
+
+        for b, i in zip(range(self.__offsets[segment], self.__offsets[segment]+self.__sizes[segment]), range(0, 9**9)):
+            ''' convert num to bytes object '''
             bstr = bytes(4)
             bstr = (value).to_bytes(self.__sizes[segment], "big")
             self.__header[b] = bstr[i]
@@ -63,7 +67,14 @@ class Header:
 a = Header()
 
 #a.set("ACK",214764870)
-a.setSegment("SEQ",0xabcd1234)
+try:
+    a.setSegment("as",0xabcd1234)
+except Exception as e:
+    print(e)
 
-z = bytes(a.setFlag(1,1))
-print(z.hex())
+print(a.header())
+'''
+z = bytes([a.setFlag(1,1)])
+print(z)
+print(type(z))
+'''

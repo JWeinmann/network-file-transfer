8137b1069add70095bdd8d521620a43201e23d97
jweinmann <weinmann@uleth.ca>
2019-11-20 12:35:48 -0700

    setFlag constrants added

--- Header.py
+++ Header.py
@@ -27,6 +27,9 @@ class Header:
             "SHA": 32,
             "DATA": 0 # default packet has no data
         }
+        ''' order of flags in __flags is inportant. Ex: flag in __flags[0] is the 1st bit, and so on '''
+        self.__flags = ["FIN","***PUT OTHER FLAGS HERE***"] # put more flags in this list
+
 
     def header(self) -> bytearray:
         return self.__header
@@ -37,16 +40,14 @@ class Header:
     ''' value ex: 2914, or 0x9a'''
     def setSegment(self, segment: str, value: int) -> None:
         if type(segment) != str:
-            raise Exception(f'Provided segment label \"{segment}\" is of type {type(segment)}. It must be of type {type("a")}. Aborting execution of Header.setSegment()')
+            raise Exception(f'Provided segment label \"{segment}\" is of type {type(segment)}. It must be of type {type("a")}.')
+        segment = segment.upper()
         ''' segment must be valid '''
-        if segment not in ["SEQ","ACK","LENGTH","WINDOW","FLAGS","SHA","DATA"]:
-            raise Exception(f'\"{segment}\" is not a valid segment. Aborting execution of Header.setSegment()')
-        elif segment in ["FLAGS","SHA","DATA"]:
-            raise Exception(f'\"{segment}\" cannot be set using Header.setSegment(). Aborting execution of Header.setSegment()')
-        ''' constrain value to what's required for segment '''
+        if segment not in ["SEQ","ACK","LENGTH","WINDOW"]:
+            raise Exception(f'\"{segment}\" is not a valid segment for setSegment().')
         if value >= 2**(8*self.__sizes[segment]) | value < 0:
             #raise: Exception(f"The provided value is {len(bstr)} bytes. The value for {segment} cannot be larger than {self._sizes[segment]} bytes.")
-            raise Exception(f'Invalid value for {segment}. Must be  between 0 and {self.__sizes[segment]}. Aborting execution of Header.setSegment()')
+            raise Exception(f'Invalid value for {segment}. Must be int between 0 and {self.__sizes[segment]}. Aborting execution of Header.setSegment()')
 
         for b, i in zip(range(self.__offsets[segment], self.__offsets[segment]+self.__sizes[segment]), range(0, 9**9)):
             ''' convert num to bytes object '''
@@ -54,8 +55,26 @@ class Header:
             bstr = (value).to_bytes(self.__sizes[segment], "big")
             self.__header[b] = bstr[i]
 
+    ''' value must be True or False '''
     def setFlag(self, flag: str, value: bool) -> None:
+        if type(flag) != str:
+            raise Exception(f'Provided flag label \"{flag}\" is of type {type(flag)}. It must be of type {type("a")}.')
         flag = flag.upper()
+        ''' segment must be valid '''
+        if flag not in ["FIN"]:
+            raise Exception(f'\"{flag}\" is not a valid flag.')
+        elif flag not in ["FIN"]:
+            raise Exception(f'\"{flag}\" cannot be set using Header.setFlag(). Aborting execution of Header.setFlag()')
+        ''' constrain value to what's required for segment '''
+        if not isinstance(value, (int, bool)):
+            #raise: Exception(f"The provided value is {len(bstr)} bytes. The value for {segment} cannot be larger than {self._sizes[segment]} bytes.")
+            raise Exception(f'Invalid value for {flag}. Must be boolean. Aborting execution of Header.setFlag()')
+        if not (0 <= value <= 1):
+            print(f'Caution: value {value} in Header.setFlag automatically interpretted as {bool(value)}.')
+
+
+
+
         b = self.__header[self.__offsets["FLAGS"]]
         return b
 
@@ -68,7 +87,7 @@ a = Header()
 
 #a.set("ACK",214764870)
 try:
-    a.setSegment(3,4294967293)
+    a.setFlag("fin",2)
 except Exception as e:
     print(e)
 
